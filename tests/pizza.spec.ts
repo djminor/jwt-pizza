756ad39fe9f67d31453355e3d74960d16da4a0db
DJ Sproul <djminor@byu.edu>
2026-02-09 17:18:41 -0700

    Added login with wrong password and register user tests. Will modulate and mock later when I know what needs to be mocked per test

--- tests/pizza.spec.ts
+++ tests/pizza.spec.ts
@@ -21,3 +21,29 @@ test('purchase with login', async ({ page }) => {
     await page.getByRole('button', { name: 'Login' }).click();
     await page.getByRole('button', { name: 'Pay now' }).click();
 });
+
+test('login with wrong password should fail', async ({ page }) => {
+    await page.goto('http://localhost:5173/');
+    await page.getByRole('link', { name: 'Login' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).fill('t@jwt.com');
+    await page.getByRole('textbox', { name: 'Email address' }).press('Tab');
+    await page.getByRole('textbox', { name: 'Password' }).fill('terst');
+    await page.getByRole('textbox', { name: 'Password' }).press('Enter');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await page.getByText('{"code":404,"message":"').click();
+});
+
+test('register user', async ({ page }) => {
+    await page.goto('http://localhost:5173/');
+    await page.getByRole('link', { name: 'Register' }).click();
+    await page.getByRole('textbox', { name: 'Full name' }).fill('blup');
+    await page.getByRole('textbox', { name: 'Email address' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).fill('blup@blup.blup');
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill('blup');
+    await page.getByRole('button', { name: 'Register' }).click();
+    await page.getByRole('link', { name: 'b', exact: true }).click();
+    await page.getByText('blup', { exact: true }).click();
+    await page.getByText('blup@blup.blup').click();
+    await page.getByText('diner', { exact: true }).click();
+});
