b53e89c055f38ddc67e4776d4831b7a6eea67fd7
DJ Sproul <djminor@byu.edu>
2026-02-10 15:33:49 -0700

    Admin can close franchise

--- tests/pizza.spec.ts
+++ tests/pizza.spec.ts
@@ -109,5 +109,13 @@ test('admin can create franchises', async ({ page }) => {
 });
 
 test('admin can close franchises', async ({ page }) => {
-    
+    await page.goto('http://localhost:5173/');
+    await page.getByRole('link', { name: 'Login' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).fill('a@jwt.com');
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill('admin');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await page.getByRole('link', { name: 'Admin' }).click();
+    await page.getByRole('row', { name: 'todelete pizza franchisee Close' }).getByRole('button').click();
+    await page.getByRole('button', { name: 'Close' }).click();
 });
