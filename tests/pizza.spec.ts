e78a22b60460c0eb62c52ee2eda5db496bcba93f
DJ Sproul <djminor@byu.edu>
2026-02-10 15:30:17 -0700

    Admins can create franchises

--- tests/pizza.spec.ts
+++ tests/pizza.spec.ts
@@ -93,6 +93,21 @@ test('not found page loads', async ({ page }) => {
 });
 
 test('admin can create franchises', async ({ page }) => {
+    await page.goto('http://localhost:5173/');
+    await page.getByRole('link', { name: 'Login' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).fill('a@jwt.com');
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill('admin');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await page.getByRole('link', { name: 'Admin' }).click();
+    await page.getByRole('button', { name: 'Add Franchise' }).click();
+    await page.getByRole('textbox', { name: 'franchise name' }).click();
+    await page.getByRole('textbox', { name: 'franchise name' }).fill('blup');
+    await page.getByRole('textbox', { name: 'franchisee admin email' }).click();
+    await page.getByRole('textbox', { name: 'franchisee admin email' }).fill('f@jwt.com');
+    await page.getByRole('button', { name: 'Create' }).click();
 });
 
-test('admin can close franchises', async ({ page }) => {});
+test('admin can close franchises', async ({ page }) => {
+    
+});
