5652c5813572a21e277f3202796d34582325fbfa
DJ Sproul <djminor@byu.edu>
2026-02-10 15:50:43 -0700

    Franchisee can access dashboard

--- tests/pizza.spec.ts
+++ tests/pizza.spec.ts
@@ -1,3 +1,4 @@
+import { time } from 'node:console';
 import { test, expect } from 'playwright-test-coverage';
 
 test('home page', async ({ page }) => {
@@ -72,7 +73,17 @@ test('logging in as admin gives access to admin dashboard', async ({ page }) => 
     await page.getByRole('link', { name: 'admin-dashboard' }).click();
 });
 
-test('user can create stores', async ({ page }) => {});
+test('franchisee can access dashboard', async ({ page }) => {
+    await page.goto('http://localhost:5173/');
+    await page.getByRole('link', { name: 'Login' }).click();
+    await page.getByRole('textbox', { name: 'Email address' }).fill('f@jwt.com');
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill('franchisee');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await expect(page.locator('#navbar-dark')).toContainText('Franchise');
+    await page.getByLabel('Global').getByRole('link', { name: 'Franchise' }).click();
+    await expect(page.getByRole('list')).toContainText('franchise-dashboard');
+});
 
 test('about page loads', async ({ page }) => {
     await page.goto('http://localhost:5173/');
@@ -116,6 +127,12 @@ test('admin can close franchises', async ({ page }) => {
     await page.getByRole('textbox', { name: 'Password' }).fill('admin');
     await page.getByRole('button', { name: 'Login' }).click();
     await page.getByRole('link', { name: 'Admin' }).click();
-    await page.getByRole('row', { name: 'todelete pizza franchisee Close' }).getByRole('button').click();
+    await page.getByRole('button', { name: 'Add Franchise' }).click();
+    await page.getByRole('textbox', { name: 'franchise name' }).click();
+    await page.getByRole('textbox', { name: 'franchise name' }).fill('todelete');
+    await page.getByRole('textbox', { name: 'franchisee admin email' }).click();
+    await page.getByRole('textbox', { name: 'franchisee admin email' }).fill('f@jwt.com');
+    await page.getByRole('button', { name: 'Create' }).click();
+    await page.getByRole('row', { name: 'todelete pizza franchisee' }).getByRole('button').click();
     await page.getByRole('button', { name: 'Close' }).click();
 });
